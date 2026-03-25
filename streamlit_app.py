ea9c438708cafeac5eaf13041e0a6fac505f65ad
Lakshmi3002 <klakshmisri2003@gmail.com>
2026-03-25 18:34:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,8 @@ my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col("FRUIT
 
 # ✅ IMPORTANT: already pandas
 pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 # -----------------------------
 # Ingredient Selector
