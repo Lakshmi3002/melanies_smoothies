e83647ff216adaeffb7a16e60ca781574272dda5
Lakshmi3002 <klakshmisri2003@gmail.com>
2026-03-25 18:04:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ fruit_df = (
     session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")
            .select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+st.stop())
            
 
 ingredients_list = st.multiselect(
